d60f20095a8e91c261846369d714245e0006f6f7
Collux <marcocolucci025@gmail.com>
2025-10-11 17:10:06 +0200

    Fix: configurazione Babel corretta

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,9 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      // Se usi react-native-reanimated, tieni questa riga:
+      'react-native-reanimated/plugin',
+    ],
   };
 };
